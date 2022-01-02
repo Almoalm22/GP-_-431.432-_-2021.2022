5d86d9664362be969288afff0fab6825d3301329
Mustafa9980 <mut22aaff@gmail.com>
2022-01-02 19:01:36 +0300

    Add a camera view to the user's home screen

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, Response
 
 from flask import Flask,render_template,request,redirect,session,flash,url_for
 from functools import wraps
@@ -62,6 +63,30 @@ def reg():
         return redirect('login')
     return render_template("reg.html",status=status)
 
+
+
+
+
+camera = cv2.VideoCapture(0)  # use 0 for web camera
+
+
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 #Home page
 @app.route("/home")
 @is_logged_in
