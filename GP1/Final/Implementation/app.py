2ff17110fd50c7adbc577dae29d76ab06597aa5c
Almoalm22 <ma146g@gmail.com>
2022-01-02 19:39:04 +0300

    sort v2.0

--- GP1/Final/Implementation/app.py
+++ GP1/Final/Implementation/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, Response
 
 from flask import Flask,render_template,request,redirect,session,flash,url_for
+from flask import Flask, render_template, Response
 from functools import wraps
 from flask_mysqldb import MySQL
 import cv2
@@ -9,7 +9,7 @@ import cv2
 app=Flask(__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_PASSWORD']='2525'
 app.config['MYSQL_DB']='db_sample'
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 mysql=MySQL(app)
@@ -62,13 +62,9 @@ def reg():
         flash('Registration Successfully. Login Here...','success')
         return redirect('login')
     return render_template("reg.html",status=status)
-
-
-
-
-
+    
 camera = cv2.VideoCapture(0)  # use 0 for web camera
-
+# for local webcam use cv2.VideoCapture(0)
 
 def gen_frames():  # generate frame by frame from camera
     while True:
@@ -87,6 +83,8 @@ def gen_frames():  # generate frame by frame from camera
 def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 #Home page
 @app.route("/home")
 @is_logged_in
